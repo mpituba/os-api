0e513e8445234bf41d9086b75f1c4505d710481c
mpituba <mpituba@Ubuntu-MPS-01>
2023-02-14 18:32:15 -0300

    Estado entity created.

---
+++ src/main/java/Endereco.java
@@ -0,0 +1,31 @@
+import br.com.mpituba.osapi.domain.model.Cidade;
+import br.com.mpituba.osapi.domain.model.Estado;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Endereco {
+	
+	@EqualsAndHashCode.Include
+	private Long id;
+	
+	private String cep;
+	
+	private String logradouro;
+	
+	private String numero;
+	
+	private String complemento;
+	
+	private String bairro;
+	
+	private Cidade cidade;
+	
+	private Estado estado;
+	
+	private String pais;
+	
+}
