2bd1df58363bfed7fce8c053120b9ae8146c2adf
mpituba <mpituba@Ubuntu-MPS-01>
2023-02-14 18:38:30 -0300

    Endereco package location changed to correct location.

--- src/main/java/br/com/mpituba/osapi/domain/model/Endereco.java
+++ src/main/java/br/com/mpituba/osapi/domain/model/Endereco.java
@@ -1,5 +1,4 @@
-import br.com.mpituba.osapi.domain.model.Cidade;
-import br.com.mpituba.osapi.domain.model.Estado;
+package br.com.mpituba.osapi.domain.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
