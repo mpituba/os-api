a3e1750052996e7841fc0d3f5a23c9ff80ab69d3
mpituba <mpituba@Ubuntu-MPS-01>
2023-02-14 15:10:28 -0300

    1.1 - Spring Initializer Commit

---
+++ src/test/java/br/com/mpituba/osapi/OsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mpituba.osapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
